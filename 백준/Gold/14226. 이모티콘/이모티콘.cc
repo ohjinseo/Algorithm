f6213398f56f03d9924a377db8477b439ad6b661
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2022-07-21 02:32:00 +0900

    [Gold IV] Title: 이모티콘, Time: 12 ms, Memory: 99808 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.cc
+++ 백준/Gold/14226. 이모티콘/이모티콘.cc
@@ -5,25 +5,6 @@ int s, clip, screen, t;
 bool visited[10000][10000];
 int result;
 
-/* dfs 시간초과
-void dfs(int clip, int screen, int time) {
-	if (result <= time || screen > s) return;
-
-	if (screen == s) {
-		result = time;
-		return;
-	}
-
-	if (clip != 0) {
-		dfs(clip, screen + clip, time + 1); // (2) 클립보드 화면에 붙여넣기
-	}
-
-	dfs(screen, screen, time + 1); // (1) 복사
-
-	dfs(clip, screen - 1, time + 1); // (3) 화면 이모티콘 삭제
-}
-*/
-
 /*
 반례 : 872
 정답 : 22
